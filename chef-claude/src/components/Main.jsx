42ab857fa852a46565f49a128919485187339000
Vanya <vbozadzhieva29@gmail.com>
2025-06-21 14:29:54 +0300

    add form functionality

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,14 +1,57 @@
+import { useState } from "react"
+
 export default function Main() {
+
+    const [ingredients, setIngredients] = useState([])
+    const [recipeShown, setRecipeShown] = useState(false)
+
+    function toggleRecipeShown() {
+        setRecipeShown(prevShown => !prevShown)
+    }
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form action={addIngredient} className="add-ingredient-form">
                 <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+
+            {ingredients.length > 0 &&
+                <section>
+                    <h2>Ingredients on hand:</h2>
+                    <ul className="ingredients-list" aria-live="polite">
+                        {ingredientsListItems}
+                    </ul>
+
+                    {ingredients.length >= 4 &&
+                        <div className="get-recipe-container">
+                            <div>
+                                <h3>Ready for a recipe?</h3>
+                                <p>Generate a recipe from your list of ingredients.</p>
+                            </div>
+                            <button onClick={toggleRecipeShown}>Get a recipe</button>
+                        </div>}
+                </section>}
+
+            {recipeShown &&
+                <section>
+                    <h2>Chef Claude Recommends:</h2>
+                    <p>Recipe from an API goes here</p>
+                </section>}
         </main>
     )
 }
