a10c89aefc2a35780da70b0c8aa301f9e2b8a9ca
Vanya <vbozadzhieva29@gmail.com>
2025-06-23 13:40:09 +0300

    Update Main.jsx

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import IngredientsList from "./IngredientsList"
 import ClaudeRecipe from "./ClaudeRecipe"
-import { getRecipeFromMistral } from "../api/getRecipe"
+import { getRecipeFromMistral } from "../ai"
 
 export default function Main() {
 
