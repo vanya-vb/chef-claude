c1fec073954f856fd479d70c0cb46d0583e99075
Vanya <vbozadzhieva29@gmail.com>
2025-06-23 15:09:15 +0300

    fix import

--- chef-claude/api/getRecipe.js
+++ chef-claude/api/getRecipe.js
@@ -1,8 +1,8 @@
-import { HfInference } from '@huggingface/inference'
+import { InferenceClient } from '@huggingface/inference'
 
 const SYSTEM_PROMPT = `You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page`
 
-const hf = new HfInference(process.env.HF_ACCESS_TOKEN)
+const hf = new InferenceClient(process.env.HF_ACCESS_TOKEN)
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -19,6 +19,7 @@ export default function Main() {
 
             const data = await response.json();
             setRecipe(data.recipe);
+            
         } catch (error) {
             console.error("Failed to get recipe:", error);
         }
